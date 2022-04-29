6ce1f8edac2b1344ac98ab603217a5e4f0b0cca2
Kristopher Roy <kristopher.roy@gmail.com>
2022-04-29 14:03:40 -0700

    modify variables to remove blank lines, added boolean for True False Tested and moving to production versions

--- Create-O365Distro.ps1
+++ Create-O365Distro.ps1
@@ -1,4 +1,4 @@
-$ver = '0.06'
+$ver = '1'
 <#
 Created By: BTL - Kristopher Roy
 Created On: 29APR22
@@ -37,16 +37,17 @@ foreach($dl in $importedList){}
 
 foreach($dl in $importedList)
 {
-    $Managedby = $dl.ManagedByAddresses.Split(',')
-    $Members = $dl.Members.Split(',')
-	$addresses = ($dl.SmtpAddresses+','+$dl.x500).split(',')
+    $Managedby = $dl.ManagedByAddresses.Split(',')|where{$_}
+    $Members = $dl.Members.Split(',')|where{$_}
+	$addresses = ($dl.SmtpAddresses+','+$dl.x500).split(',')|where{$_}
+	if($dl.RequireSenderAuthenticationEnabled -eq 'False'){$reqauth = 0}ELSEIF($dl.RequireSenderAuthenticationEnabled -eq 'True'){$reqauth = 1}
     If ($dl.GroupType -eq 'Universal' -or $dl.GroupType -eq 'Global')
     {
-        New-DistributionGroup -Name $dl.DisplayName -DisplayName $dl.DisplayName -Alias $dl.Alias -ManagedBy $Managedby -Members $Members -MemberDepartRestriction Closed -MemberJoinRestriction ApprovalRequired -PrimarySmtpAddress $dl.PrimarySmtpAddress -RequireSenderAuthenticationEnabled $dl.RequireSenderAuthenticationEnabled
+        New-DistributionGroup -Name $dl.DisplayName -DisplayName $dl.DisplayName -Alias $dl.Alias -ManagedBy $Managedby -Members $Members -MemberDepartRestriction Closed -MemberJoinRestriction ApprovalRequired -PrimarySmtpAddress $dl.PrimarySmtpAddress -RequireSenderAuthenticationEnabled $reqauth
     }
     If ($dl.GroupType -eq 'Universal, SecurityEnabled' -or $dl.GroupType -eq 'Global, SecurityEnabled')
     {
-        New-DistributionGroup -Name $dl.DisplayName -DisplayName $dl.DisplayName -Alias $dl.Alias -ManagedBy $Managedby -Members $Members -MemberDepartRestriction Closed -MemberJoinRestriction ApprovalRequired -PrimarySmtpAddress $dl.PrimarySmtpAddress -RequireSenderAuthenticationEnabled $dl.RequireSenderAuthenticationEnabled -Type "Security"
+        New-DistributionGroup -Name $dl.DisplayName -DisplayName $dl.DisplayName -Alias $dl.Alias -ManagedBy $Managedby -Members $Members -MemberDepartRestriction Closed -MemberJoinRestriction ApprovalRequired -PrimarySmtpAddress $dl.PrimarySmtpAddress -RequireSenderAuthenticationEnabled $reqauth -Type "Security"
     }
     Set-DistributionGroup $dl.DisplayName -emailaddresses @{Add=$addresses}
 }
